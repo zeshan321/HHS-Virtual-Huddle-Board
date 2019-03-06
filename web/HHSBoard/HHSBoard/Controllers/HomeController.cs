3353016954fd5f7cc5d15167649166230d146283
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-03-06 16:58:20 -0500

    Add db and usermanager

--- web/HHSBoard/HHSBoard/Controllers/HomeController.cs
+++ web/HHSBoard/HHSBoard/Controllers/HomeController.cs
@@ -5,13 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HHSBoard.Models;
+using Microsoft.AspNetCore.Authorization;
+using HHSBoard.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace HHSBoard.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private ApplicationDbContext _applicationDbContext;
+        private UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext applicationDbContext, UserManager<ApplicationUser> userManager)
         {
+            _applicationDbContext = applicationDbContext;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            ViewBag.name = user.Email;
+
             return View();
         }
 

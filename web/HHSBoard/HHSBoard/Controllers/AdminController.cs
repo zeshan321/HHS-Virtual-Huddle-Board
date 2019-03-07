656696e197a49670c1f5ede4e36be652a206dd0e
A-Summer-Day <47200874+A-Summer-Day@users.noreply.github.com>
2019-03-06 22:49:55 -0500

    User roles created
    User roles created
    Admin page created

---
+++ web/HHSBoard/HHSBoard/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HHSBoard.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        
+    }
+}
--- web/HHSBoard/HHSBoard/Startup.cs
+++ web/HHSBoard/HHSBoard/Startup.cs
@@ -33,6 +33,42 @@ namespace HHSBoard
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings 
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 6;
+
+                // Lockout settings 
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings 
+                options.User.RequireUniqueEmail = true;
+            });
+
+            //Setting the Account Login page 
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings 
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.LoginPath = "/Account/Login"; // If the LoginPath is not set here,  
+                                                      // ASP.NET Core will default to /Account/Login 
+                options.LogoutPath = "/Account/Logout"; // If the LogoutPath is not set here,  
+                                                        // ASP.NET Core will default to /Account/Logout 
+                options.AccessDeniedPath = "/Account/AccessDenied"; // If the AccessDeniedPath is  
+                                                                    // not set here, ASP.NET Core  
+                                                                    // will default to  
+                                                                    // /Account/AccessDenied 
+                options.SlidingExpiration = true;
+            });
+
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
@@ -40,8 +76,10 @@ namespace HHSBoard
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
         {
+            
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -63,6 +101,28 @@ namespace HHSBoard
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            createRolesandUsersAsync(services).Wait();
+        }
+
+        private async Task createRolesandUsersAsync(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            string[] roles = { "Admin", "User" };
+            
+     
+            IdentityResult roleResult;
+            foreach (var role in roles)
+            {
+                var roleExists = await RoleManager.RoleExistsAsync(role);
+                if (!roleExists)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
+
+                }
+            }
         }
     }
 }

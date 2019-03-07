161eae6199939f252b102fb71b9e1579e2e3bd76
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-03-07 13:10:02 -0500

    Database example

--- web/HHSBoard/HHSBoard/Data/ApplicationDbContext.cs
+++ web/HHSBoard/HHSBoard/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace HHSBoard.Data
         {
         }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);

e807cac6feb58f4059111b8eebe9703988b20371
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-03-07 13:10:02 -0500

    Database example

--- web/HHSBoard/HHSBoard/Controllers/AdminController.cs
+++ web/HHSBoard/HHSBoard/Controllers/AdminController.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HHSBoard.Data;
+using HHSBoard.Models;
+using HHSBoard.Models.AdminViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HHSBoard.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        [Authorize(Roles = "Admin")]
+        private ApplicationDbContext _applicationDbContext;
+        private UserManager<ApplicationUser> _userManager;
+
+        public AdminController(ApplicationDbContext applicationDbContext, UserManager<ApplicationUser> userManager)
+        {
+            _applicationDbContext = applicationDbContext;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
-        {            
-            return View();
+        {
+            var applicationUsers = _applicationDbContext.ApplicationUsers.ToList();
+            // Example of getting user with the id 'test'
+            ////_applicationDbContext.ApplicationUsers.Where(u => u.Id == "test").First();
+            // Example of checking if user with the id 'test' exists
+            ////_applicationDbContext.ApplicationUsers.Where(u => u.Id == "test").Any();
+
+            return View(new AdminIndexViewModel
+            {
+                ApplicationUsers = applicationUsers
+            });
         }
     }
 }

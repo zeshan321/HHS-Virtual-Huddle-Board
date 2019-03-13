ccea6c82428d6421a779929b611dfc5deaa029f2
jessf1 <jessicafong323@gmail.com>
2019-03-13 12:02:02 -0400

    Added units table to HHS database and created units screen to display each row from the new table

---
+++ web/HHSBoard/HHSBoard/Models/UnitsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HHSBoard.Models
+{
+    public class UnitsViewModel
+    {
+        public List<Unit> Units { get; set; }
+    }
+}

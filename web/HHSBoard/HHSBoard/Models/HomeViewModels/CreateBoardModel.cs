5f5d5ae49e306e0952116909e3a99a88e67fe22f
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-03-20 15:57:40 -0400

    Create board
    - Moved Unit to "Data" namespace.
    - Added ability to create board.

---
+++ web/HHSBoard/HHSBoard/Models/HomeViewModels/CreateBoardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HHSBoard.Models.HomeViewModels
+{
+    public class CreateBoardModel
+    {
+        public string Name { get; set; }
+
+        public int UnitID { get; set; }
+    }
+}

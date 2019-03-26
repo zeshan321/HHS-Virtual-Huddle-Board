d88004311e5f6c9e0e293c40e545e9efe0307197
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-03-26 09:24:44 -0400

    Added navigational board

---
+++ web/HHSBoard/HHSBoard/wwwroot/js/board.js
@@ -0,0 +1,33 @@
+﻿$.fn.editable.defaults.mode = 'inline';
+
+// List of registered tables in order.
+var tables = {
+    PURPOSE: 0,
+    CELEBRATION: 1
+};
+
+$(function () {
+    // Get current table
+    var tableType = getUrlParameter("TableType");
+
+    if (typeof tableType == 'undefined' || tableType == 0) {
+        tableType = 0;
+        
+        $("#navLeft").prop("disabled", true);
+    } else {
+        tableType = parseInt(tableType);
+
+        if (tableType >= Object.keys(tables).length / 2) {
+            $("#navRight").prop("disabled", true);
+        }
+    }
+
+    console.log(tableType);
+    $("#navLeft").click(function () {
+        location.href = location.href.replace("TableType=" + tableType, "TableType=" + (tableType - 1));
+    });
+
+    $("#navRight").click(function () {
+        location.href = location.href.replace("TableType=" + tableType, "TableType=" + (tableType + 1));
+    });
+});

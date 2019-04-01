e01121c5db8ba3b0ceb90c1021c8b8e3a76cc819
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-04-01 11:18:51 -0400

    Added auditting

---
+++ web/HHSBoard/HHSBoard/Data/Audit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HHSBoard.Data
+{
+    public class Audit
+    {
+        [Key]
+        public int ID { get; set; }
+        public string TableName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string KeyValues { get; set; }
+        public string OldValues { get; set; }
+        public string NewValues { get; set; }
+        public string Username { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ web/HHSBoard/HHSBoard/Data/Migrations/20190401144411_AuditUsername.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HHSBoard.Data.Migrations
+{
+    public partial class AuditUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Audits",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Audits");
+        }
+    }
+}

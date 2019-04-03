9ddcf0d5586ea31b96087e33e14fb1bf439ef923
jessf1 <jessicafong323@gmail.com>
2019-04-02 23:34:42 -0400

    Added New Improvements Opportunities table
    - Added new improvements opportunities table to sql database
    - Added view for creating a new improvment opprtunity record
    - Added view to display list of the new improvement opportunities records
    - Added crud fundtionality for table records

---
+++ web/HHSBoard/HHSBoard/Models/NewImpOpViewModels/NewImpOpViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HHSBoard.Data;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace HHSBoard.Models.NewImpOpViewModels
+{
+    public class NewImpOpViewModel
+    {
+        public int Total { get; set; }
+
+        [JsonProperty("rows")]
+        public List<NewImpOp> NewImpOps { get; set; }
+    }
+}

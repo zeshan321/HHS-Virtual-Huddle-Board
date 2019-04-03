ceb31b49fc35b88cd06f5b145e2801f780775e0f
jessf1 <jessicafong323@gmail.com>
2019-04-02 23:47:28 -0400

    Updated board javascript
    Modified buttons to navigate to new table when the right arrow is clicked

--- web/HHSBoard/HHSBoard/wwwroot/js/board.js
+++ web/HHSBoard/HHSBoard/wwwroot/js/board.js
@@ -5,12 +5,7 @@ $.fn.editableform.buttons =
 $.fn.combodate.defaults.maxYear = new Date().getFullYear();
 
 // List of registered tables in order.
-var tables = {
-    PURPOSE: 0,
-    CELEBRATION: 1,
-    WIP: 2,
-    NEWIMPOP: 3
-};
+var tables = ["PURPOSE", "CELEBRATION", "WIP", "YOU NEW TABLE HERE"];
 
 $(function () {
     // Get current table
@@ -23,7 +18,7 @@ $(function () {
     } else {
         tableType = parseInt(tableType);
 
-        if (tableType >= Object.keys(tables).length / 2) {
+        if (tableType >= Object.keys(tables).length - 1) {
             $("#navRight").prop("disabled", true);
         }
     }

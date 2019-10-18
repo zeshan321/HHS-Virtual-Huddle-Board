51c4ddacebe9217a828615a168b100e806fb18c8
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-10-18 18:14:16 -0400

    Added column sorting

---
+++ web/HHSBoard/HHSBoard/Extensions/SortingExtensions.cs
@@ -0,0 +1,32 @@
+﻿using HHSBoard.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HHSBoard.Extensions
+{
+    public static class SortingExtensions
+    {
+        public static IQueryable<T> Sort<T>(this IQueryable<T> queryable, Type type, string propertyName, string order)
+        {
+            if (string.IsNullOrEmpty(propertyName) || string.IsNullOrEmpty(order))
+            {
+                return queryable;
+            }
+
+            PropertyInfo sortProperty = type.GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if (order.Equals("asc"))
+            {
+                queryable = queryable.OrderBy(c => sortProperty.GetValue(c, null));
+            }
+            else
+            {
+                queryable = queryable.OrderByDescending(c => sortProperty.GetValue(c, null));
+            }
+
+            return queryable;
+        } 
+    }
+}
